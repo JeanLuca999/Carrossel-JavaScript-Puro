469b3856879f2c1cd076e497a76b3a513de9f322
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2020-11-03 10:44:03 -0300

    Classe da Lista Circular foi criada
    Ainda falta implementar a funcionalidade de voltar para o elemento anterior na classe

---
+++ js/ListaCircular.js
@@ -0,0 +1,35 @@
+export default class ListaCircular {
+    constructor(){
+        this.inicio = null
+        this.final = null
+        this.elementoAtual = null
+    }
+    adicionarElemento(url){   
+        if(this.inicio == null){
+            this.inicio = { url: url, proximo: this.inicio, anterior: this.inicio }
+            this.final = this.inicio
+            this.elementoAtual = this.inicio
+        } else {
+            this.final.proximo = { url: url, proximo: this.inicio, anterior: this.final }
+            this.final = this.final.proximo
+        }
+    }
+
+    adicionarConjuntoDeElementos(vetorDeUrl){
+        vetorDeUrl.forEach((elemento)=>{
+            this.adicionarElemento(elemento)
+        })
+    }
+
+    irParaProximoElemento(){
+        this.elementoAtual = this.elementoAtual.proximo
+    }
+
+    irParaElementoAnterior(){
+        this.elementoAtual = this.elementoAtual.anterior
+    }
+
+    obterUrlAtual(){
+        return this.elementoAtual.url
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,17 @@
+import ListaCircular from './ListaCircular.js'
+
+const fotos = ['./images/1.jpg', './images/2.jpg', './images/3.jpg']
+const c = new ListaCircular()
+c.adicionarConjuntoDeElementos(fotos)
+
+const $img = document.getElementById('foto-carrossel')
+
+function start(){
+    setInterval(()=>{
+        $img.setAttribute('src', c.obterUrlAtual())
+        c.irParaProximoElemento()
+        console.log('a')
+    }, 1000)
+}
+
+start()
